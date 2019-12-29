3d8b56818dfa845d27fff19ac5c88586adcb69a5
Eric <weifabing@gmail.com>
2019-12-29 21:28:18 +0800

    json 生成struct 结构
    1. 根据json代码生成struct结构

--- json/main.go
+++ json/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"strings"
 )
 
 type Base struct {
@@ -18,11 +19,67 @@ type Result struct {
 }
 
 func main() {
-	fmt.Printf("-->%+v\n", "Json 解析 测试")
-	jsonDecode()
+	//fmt.Printf("-->%+v\n", "Json 解析 测试")
+	//jsonDecode()
 
-	fmt.Printf("-->%+v\n", "Json 编码 测试")
-	jsonEncode()
+	//fmt.Printf("-->%+v\n", "Json 编码 测试")
+	//jsonEncode()
+
+	jsonObj := map[string]interface{}{
+		"code":    200,
+		"Message": "hello world",
+		"Data": map[string]interface{}{
+			"Name": "zhangsang",
+			"Age":  30,
+		},
+	}
+
+	s, _ := jsonToStruct(jsonObj, 0)
+	fmt.Printf("Struct code:\n%+v\n", s)
+
+}
+
+// 根据json对象生成 Struct
+// {"Data":{},"Message":"hello world","code":200}
+func jsonToStruct(jsonobj map[string]interface{}, index int) (string, error) {
+	var code = ""
+	if index == 0 {
+		code = fmt.Sprintf("type %s struct {", "MyStruct")
+	} else {
+		code = fmt.Sprintf("struct {")
+	}
+
+	//代码间隔
+	var textIndent = strings.Repeat("    ", index+1)
+
+	for key, value := range jsonobj {
+		var isStruct = false
+		dataType := "string"
+		switch v2 := value.(type) {
+		case string:
+			dataType = "string"
+		case int:
+			dataType = "int"
+		case map[string]interface{}:
+			isStruct = true
+			dataType, _ = jsonToStruct(v2, index+1)
+		default:
+			dataType = "string"
+		}
+
+		lowerKey := strings.ToLower(key)
+		_key := strings.Title(lowerKey)
+
+		if isStruct == false {
+			code = fmt.Sprintf("%v\n%s%-8s %-10s `json:%s`", code, textIndent, _key, dataType, lowerKey)
+		} else {
+			code = fmt.Sprintf("%v\n\t%-8s %-10s ", code, _key, dataType)
+		}
+	}
+	// 尾部代码间隔
+	textIndent = strings.Repeat("    ", index)
+	code = fmt.Sprintf("%v\n%s}\n", code, textIndent)
+	return code, nil
 }
 
 func jsonEncode() {

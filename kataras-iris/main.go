6c2c812c48c32fa45bcaa0c7a295a0d441b3c901
Eric <weifabing@gmail.com>
2019-12-29 10:58:51 +0800

    iris 添加新的功能
    1. 测试返回 字符串
    2. 测试返回json格式

--- kataras-iris/main.go
+++ kataras-iris/main.go
@@ -4,8 +4,24 @@ import "github.com/kataras/iris"
 
 func main() {
 	app := iris.New()
+	
+	// Method: POST
+	// Resource: http://localhost:8080/
 	app.Get("/", func(ctx iris.Context) {
 		ctx.HTML("<h1>Welcome</h1>")
+	})
+	
+	// same as app.Handle("GET", "/ping", [...])
+	// Method:   GET
+	// Resource: http://localhost:8080/ping
+	app.Get("/ping", func(ctx iris.Context) {
+		ctx.WriteString("pong")
+	})
+
+	// Method:   GET
+	// Resource: http://localhost:8080/hello
+	app.Get("hello", func(ctx iris.Context) {
+		ctx.JSON(iris.Map{"message": "Hello World"})
 	})
 	app.Run(iris.Addr(":8080"))
 }
